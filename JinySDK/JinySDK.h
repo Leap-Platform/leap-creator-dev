3124a51ce15b01c66bfd15b24425b556293569b8
AGODA CANDIDATE <agoda.candidate@gmail.com>
2020-03-16 17:51:57 +0530

    Initial Commit

---
+++ JinySDK/JinySDK.h
@@ -0,0 +1,19 @@
+//
+//  JinySDK.h
+//  JinySDK
+//
+//  Created by Aravind GS on 16/03/20.
+//  Copyright © 2020 Aravind GS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JinySDK.
+FOUNDATION_EXPORT double JinySDKVersionNumber;
+
+//! Project version string for JinySDK.
+FOUNDATION_EXPORT const unsigned char JinySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JinySDK/PublicHeader.h>
+
+
