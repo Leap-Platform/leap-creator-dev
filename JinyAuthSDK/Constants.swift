c24ce8074879793217bfad1de918cb8b149c2460
Ajay S <ajay@jiny.io>
2020-12-30 19:26:33 +0530

    added key to info.plist and close session when capture screen shot

--- JinyAuthSDK/Constants.swift
+++ JinyAuthSDK/Constants.swift
@@ -8,6 +8,5 @@
 
 import Foundation
 class Constants {
-    static let API_KEY: String = "5b4208c2-5f48-4fd2-8f0f-33362ca2d0ed"
-//    static let API_KEY = "0655dfd2-7d70-4bac-89dd-01aa003129e8"
+    static let API_KEY: String = Bundle.main.infoDictionary?["APP_API_KEY"] as! String
 }
