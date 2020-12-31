186c4ef2e0fb33e51f9d7d83e7fca7d6649d893e
Ajay S <ajay@jiny.io>
2020-12-31 19:34:00 +0530

    implemented permission to reject if app enters background or times out, default is 15 secs

--- JinyAuthSDK/managers/PermissionManager.swift
+++ JinyAuthSDK/managers/PermissionManager.swift
@@ -30,10 +30,14 @@ class PermissionManager: AppStateProtocol{
     var application: UIApplication
     let permissionGranted: String = "PERMISSION_GRANTED"
     let permissionRejected: String = "PERMISSION_REJECTED"
+    private var permissionTimer: Timer?
+    var timeout: TimeInterval = 15 // default
+    private var permissionAlert: UIAlertController?
     
     init(permissionListener: PermissionListener){
         self.permissionListener = permissionListener
         self.application = UIApplication.shared
+        addObservers()
     }
     
     //call start in MasterManager
@@ -41,21 +45,26 @@ class PermissionManager: AppStateProtocol{
         //seek permission to allow communication to take place
         
         DispatchQueue.main.async {
-            let alert = UIAlertController(title: "Streaming Permission ", message: "Do you permit Jiny Dashboard to stream your screen ?", preferredStyle: .alert)
+            
+            guard self.permissionTimer == nil else { return }
+            
+            self.permissionTimer = Timer.scheduledTimer(timeInterval: self.timeout, target: self, selector: #selector(self.permissionTimeout), userInfo: nil, repeats: false)
+            
+            self.permissionAlert = UIAlertController(title: "Streaming Permission ", message: "Do you permit Jiny Dashboard to stream your screen ?", preferredStyle: .alert)
 
-            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+            self.permissionAlert?.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
             
                 // update the Alfred server that permission has been granted
                 self.permissionListener.onPermissionGranted(permission: self.permissionGranted, status: true)
                 
             }))
-            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler:{ (action) in
+            self.permissionAlert?.addAction(UIAlertAction(title: "No", style: .cancel, handler:{ (action) in
                 self.permissionListener.onPermissionRejected(permnission: self.permissionRejected)
             }))
 
             
            // UIApplication.shared.keyWindow!.rootViewController?.present(alert, animated: true)
-            UIApplication.getCurrentVC()?.present(alert, animated: true)
+            UIApplication.getCurrentVC()?.present(self.permissionAlert!, animated: true)
         }
         
     }
@@ -96,6 +105,22 @@ class PermissionManager: AppStateProtocol{
     }
     func stop()->Void{
         
+    }
+    
+    func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(appDidEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    
+    @objc private func appDidEnterBackground(){
+        permissionTimeout()
+    }
+    
+    @objc func permissionTimeout() {
+        guard permissionTimer != nil else { return }
+        permissionTimer?.invalidate()
+        permissionTimer = nil
+        self.permissionAlert?.dismiss(animated: true, completion: nil)
+        self.permissionListener.onPermissionRejected(permnission: self.permissionRejected)
     }
 }
 
