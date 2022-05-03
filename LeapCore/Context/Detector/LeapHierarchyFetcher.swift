77340f3f2d6285efec2755d4ef816acc3acb4cc8
Aravind G S <aravind@jiny.io>
2022-05-03 11:35:50 +0530

    New hierarchy fetcher logic implemented

--- LeapCore/Context/Detector/LeapHierarchyFetcher.swift
+++ LeapCore/Context/Detector/LeapHierarchyFetcher.swift
@@ -14,29 +14,52 @@ class LeapHierarchyFetcher {
     func fetchHierarchy() -> [String : LeapViewProperties] {
         var hierarchy:[String : LeapViewProperties] = [:]
         let allWindows = UIApplication.shared.windows
-        for window in allWindows {
-            var props = generateLeapViewProperties(window, nil, allWindows.firstIndex(of: window) ?? 0)
-            hierarchy[props.viewId] = props
+        for (index, window) in allWindows.enumerated() {
+            let windowProps = generateLeapViewProperties(window, nil, index)
+            let windowHierachy = getChildrenHierachy(windowProps)
+            hierarchy.merge(windowHierachy) { originalProps, _ in
+                return originalProps
+            }
         }
         return hierarchy
     }
     
-    func getChildrenLeapViewProperties(_ viewProperties:LeapViewProperties) -> [String:LeapViewProperties] {
-        var props:[String : LeapViewProperties] = [:]
+    private func getChildrenHierachy(_ viewProperties:LeapViewProperties) -> [String:LeapViewProperties] {
+        let viewId = viewProperties.viewId
+        var hierarchy:[String : LeapViewProperties] = [viewId : viewProperties]
         guard let view = viewProperties.weakView else { return [:] }
-        for subview in view.subviews {
-            let subviewProps = generateLeapViewProperties(subview, viewProperties.viewId, view.subviews.firstIndex(of: subview)!)
-            props[subviewProps.viewId] = subviewProps
-            viewProperties.children.append(subviewProps.viewId)
-            let childProps = getChildrenLeapViewProperties(subviewProps)
-            props.merge(childProps) { originalDictProps, newDictProps in
-                return originalDictProps
+        for subview in validSubviewsFor(view) {
+            let viewIndex = view.subviews.firstIndex(of: subview)!
+            let subviewProps = generateLeapViewProperties(subview, viewId, viewIndex)
+            hierarchy[viewId]?.children.append(subviewProps.viewId)
+            let subviewHierarchy = getChildrenHierachy(subviewProps)
+            hierarchy.merge(subviewHierarchy) { originalProps, _ in
+                return originalProps
             }
         }
-        return props
+        return hierarchy
     }
     
-    func generateLeapViewProperties(_ view: UIView, _ parentUUID:String?, _ index:Int = 0) -> LeapViewProperties {
+    private func validSubviewsFor(_ view:UIView) -> [UIView] {
+        let filteredSubviews = view.subviews.filter{ !$0.isHidden && ($0.alpha > 0)  && !String(describing: type(of: $0)).contains("Leap") }
+        let filteredVisibleSubviews = visibleChildrenFor(filteredSubviews)
+        return filteredVisibleSubviews
+    }
+    
+    private func visibleChildrenFor(_ views: Array<UIView>) -> Array<UIView> {
+        var visibleViews:Array<UIView> = views
+        for coveringView in views.reversed() {
+            if !visibleViews.contains(coveringView) { continue }
+            let index = views.firstIndex(of: coveringView)
+            if index == nil || index == 0 { continue }
+            let elderSiblings = views[0..<index!]
+            let elderSiblingsHiddenByCoveringView = elderSiblings.filter { !visibleViews.contains($0) || coveringView.frame.contains($0.frame) }
+            visibleViews = visibleViews.filter { !elderSiblingsHiddenByCoveringView.contains($0) }
+        }
+        return visibleViews
+    }
+    
+    private func generateLeapViewProperties(_ view: UIView, _ parentUUID:String?, _ index:Int) -> LeapViewProperties {
         return LeapViewProperties(with: view, uuid: UUID().uuidString, parentUUID: parentUUID, index: index)
     }
     
